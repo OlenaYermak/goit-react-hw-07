8c0614f960d0bf997ac126175a8de201ddad9328
OlenaYermak <145419464+OlenaYermak@users.noreply.github.com>
2024-05-10 08:05:11 +0300

    add project

---
+++ src/redux/store.js
@@ -0,0 +1,18 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+
+import contactsReducer from "./contactsSlice";
+import filtersReducer from "./filterSlice";
+
+
+
+const store = configureStore({
+  reducer: {
+    contacts: contactsReducer,
+    filters: filtersReducer,
+  },
+  
+});
+
+
+export default store;
